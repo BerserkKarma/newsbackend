9eab736bed2ec44a56cd0e413937662a22284e56
Medi0cr9 <immortalthang@outlook.com>
2022-10-15 21:47:13 +0800

    new lib

--- assets/js/login.js
+++ assets/js/login.js
@@ -13,25 +13,25 @@ $(function () {
 
     //表单校验，自定义规则
     //从layui中获取form对象
-    var form = layui.form
+    // var form = layui.form
     var layer = layui.layer
-    // console.log(form)
-    //通过form.verify()函数自定义校验规则
-    form.verify({
-        // 自定义了一个叫做 pwd 校验规则
-        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-        // 校验两次密码是否一致的规则
-        repwd: function (value) {
-            // 通过形参拿到的是确认密码框中的内容
-            // 还需要拿到密码框中的内容
-            // 然后进行一次等于的判断
-            // 如果判断失败,则return一个提示消息即可
-            var pwd = $('.reg-box [name=password]').val()
-            if (pwd !== value) {
-                return '两次密码不一致！'
-            }
-        }
-    })
+    // // console.log(form)
+    // //通过form.verify()函数自定义校验规则
+    // form.verify({
+    //     // 自定义了一个叫做 pwd 校验规则
+    //     pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+    //     // 校验两次密码是否一致的规则
+    //     repwd: function (value) {
+    //         // 通过形参拿到的是确认密码框中的内容
+    //         // 还需要拿到密码框中的内容
+    //         // 然后进行一次等于的判断
+    //         // 如果判断失败,则return一个提示消息即可
+    //         var pwd = $('.reg-box [name=password]').val()
+    //         if (pwd !== value) {
+    //             return '两次密码不一致！'
+    //         }
+    //     }
+    // })
     //监听注册表单提交事件
     $('#form_reg').on('submit', function (e) {
         //阻止默认行为
